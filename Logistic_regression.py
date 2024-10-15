abfbb65f2c80c04561fe417e11a5be655160c49b
Tanish Jain <116287000+Tanish-0001@users.noreply.github.com>
2024-10-15 20:41:19 +0530

    Update Logistic_regression.py

--- Logistic_regression.py
+++ Logistic_regression.py
@@ -1,12 +1,13 @@
 import numpy as np
-import matplotlib.pyplot as plt
 from sklearn.datasets import load_breast_cancer
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
 
+np.random.seed(1000)
+
 
 class LogisticRegression:
-    def __init__(self, max_iter=100, eta=0.01):
+    def __init__(self, max_iter=1000, eta=0.001):
         self.parameters = None
         self.max_iter = max_iter
         self.learning_rate = eta
@@ -14,20 +15,20 @@ class LogisticRegression:
     @staticmethod
     def sigmoid(z):
         return 1 / (1 + np.exp(-z))
-        
+
     def forward(self, X):
         return self.sigmoid(np.dot(X, self.parameters))
 
     @staticmethod
     def loss_function(yPred, yTrue):
         epsilon = 1e-15
-        yPred = np.clip(yPred, epsilon, 1 - epsilon)
+        yPred = np.clip(yPred, epsilon, 1 - epsilon)  # to prevent log(0) when y_pred == 0
         return -np.mean(yTrue * np.log(yPred) + (1 - yTrue) * np.log(1 - yPred))
 
     @staticmethod
     def gradient(X, yPred, yTrue):
         m = yTrue.shape[0]
-        dW = np.dot(X.T, (yTrue - yPred)) / m
+        dW = np.dot((yTrue - yPred), X) / m
         return dW
 
     def fit(self, xTrain, yTrain):
@@ -35,19 +36,19 @@ class LogisticRegression:
         XTrain = np.insert(xTrain, 0, 1, axis=1)
 
         for epoch in range(self.max_iter):
-            Y_pred = self.forward(XTrain, self.parameters)
+            Y_pred = self.forward(XTrain)
             _loss = self.loss_function(Y_pred, yTrain)
             dw = self.gradient(XTrain, Y_pred, yTrain)
             self.parameters += self.learning_rate * dw
 
             if epoch % (self.max_iter / 10) == 0:
-                print(f"epoch: {epoch}, loss: {_loss:.5f}")
+                print(f"epoch: {epoch}, loss: {_loss}")
 
     def predict(self, xTest):
         XTest = np.insert(xTest, 0, 1, axis=1)
-        yTestPred = self.forward(XTest, self.parameters)
+        yTestPred = self.forward(XTest)
         return yTestPred
-
+        
 
 data = load_breast_cancer()
 x, y = data['data'], data['target']
@@ -65,3 +66,17 @@ y_test_pred = model.predict(x_test)
 y_test_pred_class = np.array([1 if i > 0.5 else 0 for i in y_test_pred])  # threshold probability = 0.5
 accuracy = np.mean(y_test_pred_class == y_test)
 print(f'Accuracy: {accuracy * 100:.2f}%')
+
+# Confusion Matrix, Precision, Recall, F1 Score
+tp = np.sum(np.logical_and(y_test_pred_class == 1, y_test == 1))
+tn = np.sum(np.logical_and(y_test_pred_class == 0, y_test == 0))
+fp = np.sum(np.logical_and(y_test_pred_class == 1, y_test == 0))
+fn = np.sum(np.logical_and(y_test_pred_class == 0, y_test == 1))
+
+print(f"Confusion matrix:\n{np.array([[tp, fn], [fp, tn]])}")
+
+precision = tp / (tp + fp)
+recall = tp / (tp + fn)
+f1_score = 2 * (precision * recall) / (precision + recall)
+
+print(f"Precision: {precision:.2f}, Recall: {recall}, F1: {f1_score:.2f}")
